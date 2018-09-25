56b4ce7e6d8c8a875ed960d642d29c3c48b760da
Gaosling.Aaron <876610383@qq.com>
2018-09-25 15:28:35 +0800

    1.实现了切换到自定义软键盘弹出的EditText时不弹出系统默认的软键盘；

--- app/src/main/java/com/custom/simplekeyboardview/KeyboardPopupWindow.java
+++ app/src/main/java/com/custom/simplekeyboardview/KeyboardPopupWindow.java
@@ -13,17 +13,34 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class KeyboardPopupWindow extends PopupWindow {
     private static final String TAG = "KeyboardPopupWindow";
     private Context context;
     private View anchorView;
+    private View parentView;
     private EditText editText;
+    private boolean isRandomSort = false;//数字是否随机排序
+    private List<Integer> list = new ArrayList<>();
+    private int[] commonButtonIds = new int[]{R.id.button00, R.id.button01, R.id.button02, R.id.button03,
+            R.id.button04, R.id.button05, R.id.button06, R.id.button07, R.id.button08, R.id.button09};
 
-    public KeyboardPopupWindow(Context context, View anchorView, EditText editText) {
+    /**
+     * @param context
+     * @param anchorView
+     * @param editText
+     * @param isRandomSort 数字是否随机排序
+     */
+    public KeyboardPopupWindow(Context context, View anchorView, EditText editText, boolean isRandomSort) {
         this.context = context;
         this.anchorView = anchorView;
         this.editText = editText;
+        this.isRandomSort = isRandomSort;
         if (context == null || anchorView == null) {
             return;
         }
@@ -31,12 +48,39 @@ public class KeyboardPopupWindow extends PopupWindow {
         initView();
     }
 
+
     private void initConfig() {
         setOutsideTouchable(false);
         setFocusable(false);
         setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        forbidDefaultSoftKeyboard();
     }
 
+    /**
+     * 禁止系统默认的软键盘弹出
+     */
+    private void forbidDefaultSoftKeyboard() {
+        if (editText == null) {
+            return;
+        }
+        if (android.os.Build.VERSION.SDK_INT > 10) {//4.0以上，使用反射的方式禁止系统自带的软键盘弹出
+            try {
+                Class<EditText> cls = EditText.class;
+                Method setShowSoftInputOnFocus;
+                setShowSoftInputOnFocus = cls.getMethod("setShowSoftInputOnFocus", boolean.class);
+                setShowSoftInputOnFocus.setAccessible(true);
+                setShowSoftInputOnFocus.invoke(editText, false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 刷新自定义的popupwindow是否outside可触摸反应：如果是不可触摸的，则显示该软键盘view
+     *
+     * @param isTouchable
+     */
     public void refreshKeyboardOutSideTouchable(boolean isTouchable) {
         setOutsideTouchable(isTouchable);
         if (!isTouchable) {
@@ -49,11 +93,11 @@ public class KeyboardPopupWindow extends PopupWindow {
     }
 
     private void initView() {
-        final View view = LayoutInflater.from(context).inflate(R.layout.keyboadview, null);
-        initKeyboardView(view);
+        parentView = LayoutInflater.from(context).inflate(R.layout.keyboadview, null);
+        initKeyboardView(parentView);
         setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
         setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
-        setContentView(view);
+        setContentView(parentView);
     }
 
     private void initKeyboardView(View view) {
@@ -65,50 +109,93 @@ public class KeyboardPopupWindow extends PopupWindow {
             }
         });
 
-        //给数字键设置点击监听
-        int[] commonButtonIds = new int[]{R.id.button00, R.id.button01, R.id.button02, R.id.button03, R.id.button04,
-                R.id.button05, R.id.button06, R.id.button07, R.id.button08, R.id.button09};
+        //①给数字键设置点击监听
         for (int i = 0; i < commonButtonIds.length; i++) {
             final Button button = view.findViewById(commonButtonIds[i]);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    editText.setText(editText.getText().toString() + button.getText());
-                    editText.setSelection(editText.getText().toString().length());
+                    int curSelection = editText.getSelectionStart();
+                    int length = editText.getText().toString().length();
+                    if (curSelection < length) {
+                        String content = editText.getText().toString();
+                        editText.setText(content.substring(0, curSelection) + button.getText() + content.subSequence(curSelection, length));
+                        editText.setSelection(curSelection + 1);
+                    } else {
+                        editText.setText(editText.getText().toString() + button.getText());
+                        editText.setSelection(editText.getText().toString().length());
+                    }
                 }
             });
         }
 
-        //给点键设置点击监听
+        //②给小数点按键设置点击监听
         view.findViewById(R.id.buttonDot).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + ".");
-                editText.setSelection(editText.getText().toString().length());
-            }
-        });
-
-        //给叉键设置点击监听
-        view.findViewById(R.id.buttonCross).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                int curSelection = editText.getSelectionStart();
                 int length = editText.getText().toString().length();
-                if (length > 0) {
-                    editText.setText(editText.getText().toString().substring(0, length - 1));
-                    editText.setSelection(length - 1);
+                if (curSelection < length) {
+                    String content = editText.getText().toString();
+                    editText.setText(content.substring(0, curSelection) + "." + content.subSequence(curSelection, length));
+                    editText.setSelection(curSelection + 1);
+                } else {
+                    editText.setText(editText.getText().toString() + ".");
+                    editText.setSelection(editText.getText().toString().length());
                 }
             }
         });
 
-
+        //③给叉按键设置点击监听
+        view.findViewById(R.id.buttonCross).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int length = editText.getText().toString().length();
+                int curSelection = editText.getSelectionStart();
+                if (length > 0 && curSelection > 0 && curSelection <= length) {
+                    String content = editText.getText().toString();
+                    editText.setText(content.substring(0, curSelection - 1) + content.subSequence(curSelection, length));
+                    editText.setSelection(curSelection - 1);
+                }
+            }
+        });
     }
 
 
     public void show() {
         if (!isShowing() && anchorView != null) {
+            doRandomSortOp();
             this.showAtLocation(anchorView, Gravity.BOTTOM, 0, 0);
         }
     }
+
+    /**
+     * 随机分布数字
+     */
+    private void doRandomSortOp() {
+        if (parentView == null) {
+            return;
+        }
+        if (!isRandomSort) {
+            for (int i = 0; i < commonButtonIds.length; i++) {
+                final Button button = parentView.findViewById(commonButtonIds[i]);
+                button.setText("" + i);
+            }
+        } else {
+            list.clear();
+            Random ran = new Random();
+            while (list.size() < commonButtonIds.length) {
+                int n = ran.nextInt(commonButtonIds.length);
+                if (!list.contains(n))
+                    list.add(n);//如果n不包涵在list中，才添加
+            }
+            for (int i = 0; i < commonButtonIds.length; i++) {
+                final Button button = parentView.findViewById(commonButtonIds[i]);
+                button.setText("" + list.get(i));
+            }
+        }
+    }
+
 
     public void refreshViewAndShow(Context context, View anchorView, EditText editText) {
         this.context = context;
@@ -120,10 +207,22 @@ public class KeyboardPopupWindow extends PopupWindow {
         show();
     }
 
+    public boolean isRandomSort() {
+        return isRandomSort;
+    }
+
+    public void setRandomSort(boolean randomSort) {
+        isRandomSort = randomSort;
+    }
+
     public void releaseResources() {
         this.dismiss();
         context = null;
         anchorView = null;
+        if (list != null) {
+            list.clear();
+            list = null;
+        }
     }
 
 }
--- app/src/main/java/com/custom/simplekeyboardview/MainActivity.java
+++ app/src/main/java/com/custom/simplekeyboardview/MainActivity.java
@@ -3,13 +3,10 @@ package com.custom.simplekeyboardview;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-
-import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -26,12 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         numberEt = findViewById(R.id.numberEt);
-        keyboardPopupWindow = new KeyboardPopupWindow(MainActivity.this, getWindow().getDecorView(), numberEt);
+        keyboardPopupWindow = new KeyboardPopupWindow(MainActivity.this, getWindow().getDecorView(), numberEt,true);
 //        numberEt.setInputType(InputType.TYPE_NULL);//该设置会导致光标不可见
         numberEt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hideInput(MainActivity.this, getWindow().getDecorView());//传递整个窗口的view，而不是具体某个
                 if (keyboardPopupWindow != null) {
                     keyboardPopupWindow.show();
                 }
@@ -42,10 +38,14 @@ public class MainActivity extends AppCompatActivity {
             public void onFocusChange(View v, boolean hasFocus) {
                 Log.d(TAG, "数字输入框是否有焦点——>" + hasFocus);
                 if (keyboardPopupWindow != null && isUiCreated) {//很重要，Unable to add window -- token null is not valid; is your activity running?
-                    // 需要等待页面创建完成后焦点变化才去显示自定义键盘
-                    keyboardPopupWindow.refreshKeyboardOutSideTouchable(!hasFocus);
+                    keyboardPopupWindow.refreshKeyboardOutSideTouchable(!hasFocus);// 需要等待页面创建完成后焦点变化才去显示自定义键盘
                 }
-                hideInput(MainActivity.this, getWindow().getDecorView());//传递整个窗口的view，而不是具体某个
+
+                if (hasFocus) {//隐藏系统软键盘
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(numberEt.getWindowToken(), 0);
+                }
+
             }
         });
     }
@@ -54,12 +54,6 @@ public class MainActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         isUiCreated = true;
-    }
-
-    private void hideInput(Context context, View view) {
-        InputMethodManager inputMethodManager =
-                (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 
